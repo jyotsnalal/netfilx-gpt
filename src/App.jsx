8e05d799a82ada50fcfb6ac4da6e0fcac8677bf3
Jyotsnalal <das.jyotsna13@gmail.com>
2025-12-23 16:45:56 +0530

    Resolve merge conflict and add Redux provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import React from 'react'
-<<<<<<< HEAD
-import Body from './components/Body'
-const App = () => {
-  return (
-    <div>
-     
-     <Body/>
-=======
-import {Provider} from "react-redux";
-import Body from './components/Body'
-import appStore from './utils/appStore';
+import React from "react";
+import { Provider } from "react-redux";
+import Body from "./components/Body";
+import appStore from "./utils/appStore";
 
 const App = () => {
   return (
-    <div>
-     <Provider store={appStore}>
-     <Body/>
-     </Provider>
->>>>>>> 5781ccb (validation && redux && auth)
+    <Provider store={appStore}>
+      <Body />
+    </Provider>
+  );
+};
 
-    </div>
-  )
-}
-
-export default App
+export default App;
